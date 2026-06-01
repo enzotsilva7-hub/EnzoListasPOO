4cd1211bea53ebfcdda2027be91c7e3e32d73f60
enzotsilva7-hub <enzo.t.silva7@aluno.senai.br>
2026-06-01 19:24:10 -0400

    Primeiro commit

---
+++ Exercicio1/main.java
@@ -0,0 +1,13 @@
+package Exercicio1;
+
+public class main {
+    public static void main (String [args]) {
+        int nota = 22;
+        Integer notaObj = nota;
+
+        System.out.println("Valor da variavel:" +nota);
+        System.out.println("Valor da variavel:"+notaObj);
+        System.out.println("Valor da variavel:"+notaObj.getClass(). getSimpleName());
+
+    }
+}
