13459cb03e91d9fb8a66a771d7a58f85655e2dd1
enzotsilva7-hub <enzo.t.silva7@aluno.senai.br>
2026-06-01 19:30:20 -0400

    Segundo commit

---
+++ Exercicio2/main.java
@@ -0,0 +1,9 @@
+package Exercicio2;
+
+public class main {
+
+    public static void main(String[] args) {
+        Integer x = null;
+        int numero = x;
+        System.out.println(numero);
+    }
